5802694e4f8d62c29f707b800dec063ca9841b3a
Thomas Hutterer <thutterer@suse.de>
2018-01-20 17:34:36 +0100

    :clock2:
    Add option to (not) show seconds.

--- clock.js
+++ clock.js
@@ -1,6 +1,15 @@
 function updateClock() {
-  document.getElementById('clock').innerHTML = (new Date).toTimeString().slice(0, 8)
+  var now = new Date;
+  time.innerHTML = now.toTimeString().slice(0, (secsSetting.checked ? 8 : 5))
+  date.innerHTML = dateSetting.checked ? now.toDateString().slice(0, 16).replace(/ /g, '!') : ''
 }
+
+var time = document.getElementById('time')
+var secsSetting = document.getElementById('show_seconds')
+var dateSetting = document.getElementById('show_date')
+
+secsSetting.onclick = updateClock;
+dateSetting.onclick = updateClock;
 
 updateClock()
 setInterval(updateClock, 1000)
